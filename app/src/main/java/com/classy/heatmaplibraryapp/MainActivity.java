8404926caed1d857c13e248a65a83bc388db8c78
guymacin <guyskv@gmail.com>
2020-11-09 20:57:35 +0200

    Initial commit

---
+++ app/src/main/java/com/classy/heatmaplibraryapp/MainActivity.java
@@ -0,0 +1,115 @@
+package com.classy.heatmaplibraryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.classy.heatmaplibrary.HeatMap;
+import com.google.android.material.button.MaterialButton;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout main_LAY_root;
+    private ImageView main_IMG_heatMap;
+    private MaterialButton main_BTN_start;
+    private ProgressBar main_PRG_heat;
+
+    private double[][] screenData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        main_LAY_root = findViewById(R.id.main_LAY_root);
+        main_IMG_heatMap = findViewById(R.id.main_IMG_heatMap);
+        main_BTN_start = findViewById(R.id.main_BTN_start);
+        main_PRG_heat = findViewById(R.id.main_PRG_heat);
+        main_PRG_heat.setVisibility(View.GONE);
+
+        main_BTN_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start();
+            }
+        });
+
+        calculateTouchScreenResolution(main_LAY_root);
+
+        int width  = main_LAY_root.getMeasuredWidth();
+        int height = main_LAY_root.getMeasuredHeight();
+    }
+
+    private void calculateTouchScreenResolution(View view) {
+        ViewTreeObserver vto = view.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                int width  = view.getMeasuredWidth();
+                int height = view.getMeasuredHeight();
+                Log.d("pttt", "width= " + width + ", height= " + height);
+                initScreenListener(view, height, width);
+            }
+        });
+    }
+
+    private void initScreenListener(View view, int height, int width) {
+        screenData = new double[height][width];
+
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("pttt", "Action: " + event.getAction());
+
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                //screenData[y][x] += 0.1;
+
+
+                int R = 10;
+                for (int i = -R; i <= R; i++) {
+                    for (int j = -R; j <= R; j++) {
+                        double val = Math.sqrt(i*i + j*j);
+                        val = val == 0 ? 1.0 : 1.0 / val;
+                        screenData[y+i][x+j] += val;
+                    }
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void start() {
+        main_PRG_heat.setVisibility(View.VISIBLE);
+        main_BTN_start.setVisibility(View.GONE);
+        HeatMap.generateBitmap(screenData, new HeatMap.CallBack_HeatMap() {
+            @Override
+            public void bitmapReady(Bitmap bitmap) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        main_PRG_heat.setVisibility(View.GONE);
+                        main_IMG_heatMap.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ heatMapLibrary/src/main/java/com/classy/heatmaplibrary/Temp.java
@@ -0,0 +1,199 @@
+package com.classy.heatmaplibrary;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Temp {
+
+
+    private static double[][] normalizeMatrix(double[][] matrix) {
+        double RT = 10.0; // Ratio
+        double[][] lowResMatrix = new double[(int) (matrix.length / RT)][(int) (matrix[0].length / RT)];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                try {
+                    lowResMatrix[(int) (i / RT)][(int) (j / RT)] += matrix[i][j];
+                } catch (ArrayIndexOutOfBoundsException ex) {
+                }
+            }
+        }
+
+
+        double[][] norMatrix = new double[lowResMatrix.length][lowResMatrix[0].length];
+
+        int R = 10; // Range
+        for (int y = 0; y < lowResMatrix.length; y++) {
+            Log.d("pttt", y + " / " + lowResMatrix.length);
+            for (int x = 0; x < lowResMatrix[0].length; x++) {
+                for (int i = -R; i <= R; i++) {
+                    for (int j = -R; j <= R; j++) {
+                        try {
+                            norMatrix[y][x] = lowResMatrix[y + i][x + j];
+                        } catch (ArrayIndexOutOfBoundsException ex) {
+                        }
+                    }
+                }
+            }
+        }
+
+
+        double[][] clrMatrix = new double[220][220];
+
+        for (int i = 0; i < 220; i++) {
+            for (int j = 0; j < 220; j++) {
+                clrMatrix[i][j] = j / 220.0;
+            }
+        }
+        return clrMatrix;
+    }
+
+
+    /**
+     * this function prepare heat map bitmap.
+     * @param matrix double matrix, must be max 1.0 value
+     * @param callBack_heatMap to return after long task.
+     */
+    public static void generateBitmap(double[][] matrix, CallBack_HeatMap callBack_heatMap) {
+        //new MyLongOperation(matrix, callBack_heatMap).execute();
+
+
+        TaskRunner taskRunner = new TaskRunner();
+        LongRunningTask longRunningTask = new LongRunningTask(matrix, callBack_heatMap);
+        taskRunner.executeAsync(longRunningTask, new TaskRunner.Callback<R>() {
+            @Override
+            public void onComplete(R result) {
+
+            }
+        });
+    }
+
+    private static Bitmap generateBitmap(double[][] matrix) {
+        matrix = normalizeMatrix(matrix);
+        int HEIGHT = matrix.length;
+        int WIDTH = matrix[0].length;
+        Bitmap image = Bitmap.createBitmap(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888);
+
+        // 0.0  ------ 1.0
+
+//        int[] colors = new int[]{
+//                ContextCompat.getColor(context, R.color.heat0),
+//                ContextCompat.getColor(context, R.color.heat1),
+//                ContextCompat.getColor(context, R.color.heat2),
+//                ContextCompat.getColor(context, R.color.heat3),
+//                ContextCompat.getColor(context, R.color.heat4),
+//                ContextCompat.getColor(context, R.color.heat5),
+//                ContextCompat.getColor(context, R.color.heat6),
+//                ContextCompat.getColor(context, R.color.heat7),
+//                ContextCompat.getColor(context, R.color.heat8),
+//                ContextCompat.getColor(context, R.color.heat9)
+//        };
+
+        // https://stackoverflow.com/a/52498075/7147289
+
+        int column = 0;
+        int row = 0;
+        double val;
+        while (row < HEIGHT) {
+            while (column < WIDTH) {
+                val = matrix[row][column];
+                //image.setPixel(column, row, colors[(int) val]);
+                image.setPixel(column, row, Color.HSVToColor(new float[]{(float) (220.0 * val), 1.0f, 1.0f}));
+                column++;
+            }
+            row++;
+            column = 0;
+        }
+
+        return image;
+    }
+
+    public interface CallBack_HeatMap {
+        void bitmapReady(Bitmap bitmap);
+    }
+
+    static class MyLongOperation extends AsyncTask<Void, Void, Void> {
+
+        private double[][] matrix;
+        private CallBack_HeatMap callBack_heatMap;
+
+        public MyLongOperation(double[][] matrix, CallBack_HeatMap callBack_heatMap) {
+            this.matrix = matrix;
+            this.callBack_heatMap = callBack_heatMap;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            Bitmap bitmap = generateBitmap(matrix);
+            if (callBack_heatMap != null) {
+                callBack_heatMap.bitmapReady(bitmap);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+        }
+    }
+
+
+    public static class TaskRunner {
+        private final Executor executor = Executors.newSingleThreadExecutor(); // change according to your requirements
+        private final Handler handler = new Handler(Looper.getMainLooper());
+
+        public interface Callback<R> {
+            void onComplete(R result);
+        }
+
+        public <R> void executeAsync(Callable<R> callable, Callback<R> callback) {
+            executor.execute(() -> {
+                final R result;
+                try {
+                    result = callable.call();
+                    handler.post(() -> {
+                        callback.onComplete(result);
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+
+    static class LongRunningTask implements Callable<R> {
+
+        private double[][] matrix;
+        private CallBack_HeatMap callBack_heatMap;
+
+        public LongRunningTask(double[][] matrix, CallBack_HeatMap callBack_heatMap) {
+            this.matrix = matrix;
+            this.callBack_heatMap = callBack_heatMap;
+        }
+
+        @Override
+        public R call() {
+
+            Bitmap bitmap = generateBitmap(matrix);
+            if (callBack_heatMap != null) {
+                callBack_heatMap.bitmapReady(bitmap);
+            }
+
+            return null;
+        }
+    }
+}
